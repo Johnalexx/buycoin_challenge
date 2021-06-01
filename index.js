7badf09743aaf7abc46f1a462decf5561a4931bc
Johnalexx <adegbolajohn1@gmail.com>
2021-06-01 01:07:33 +0100

    app

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+// function DarkMode(){
+//     alert ("Johnalex is a boy");
+// }
+function DarkSwitch(){
+    alert ("Switching to Dark Mode...");
+    // document.getElementById('active').style.color="red";
+}
